252a266fdff60f89df7cdc7eabd804d0358fb0c8
YutongHan1123 <42613354+YutongHan1123@users.noreply.github.com>
2019-03-04 12:09:23 -0500

    renew

---
+++ index.js
@@ -0,0 +1,14 @@
+
+d3.select('h2').style('color','red');
+
+
+// import {select csv} from 'd3';
+//
+// const svg = select('svg');
+//
+// const width = +svg.attr('width');
+// const height = +svg.attr('height');
+//
+// csv('.../data/mlb-satndings.csv').then(data => {
+//   console.log(data);
+// });
